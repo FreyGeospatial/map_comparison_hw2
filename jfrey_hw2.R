593c4beac01e70fa20c8c6a20dbdb51b30ffab2f
jmfrey <jfrey@clarku.edu>
2019-09-08 15:44:58 -0400

    created boolean image of built areas

--- jfrey_hw2.R
+++ jfrey_hw2.R
@@ -4,3 +4,13 @@ library(TOC)
 #load data
 lc_1971 <- raster("Landcover01/1971Landcover01.rst")
 lc_1985 <- raster("Landcover01/1985Landcover01.rst")
+
+# create boolean image of "Built": 
+# reclassification matrix: is, becomes
+built <- c(1L, 1L)
+not_built <- rbind(c(2L, 0L),
+                   c(3L, 0L),
+                   c(4L, 0L))
+
+my_reclass <- rbind(built, not_built)
+built_bool <- reclassify(lc_1971, rcl = my_reclass)
