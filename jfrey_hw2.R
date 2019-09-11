c08501ad3c97718f03ed164dd039d4a054eff85a
jmfrey <jfrey@clarku.edu>
2019-09-11 11:38:21 -0400

    produced plot for Q2

--- jfrey_hw2.R
+++ jfrey_hw2.R
@@ -5,6 +5,9 @@ library(TOC)
 lc_1971 <- raster("Landcover01/1971Landcover01.rst")
 lc_1985 <- raster("Landcover01/1985Landcover01.rst")
 
+#Data prep for Q1:
+########################################################
+
 # create boolean image of "Built": 
 # reclassification matrix: is, becomes
 built <- c(1L, 1)
@@ -22,23 +25,54 @@ not_built <- rbind(c(2L, 1L),
                    c(3L, 1L), 
                    c(4L, 1L))
 my_nonbuilt_reclass <- rbind(built, not_built)
-not_built_bool_71 <- reclassify(lc_1971, rcl = my_nonbuilt_reclass)
+not_built_bool_71 <- reclassify(lc_1971, rcl = my_nonbuilt_reclass) # will be masked image
 
 #distance calculation
 distance_71 <- gridDistance(built_bool_71, 1)
 plot(distance_71) #calculates distance from all cells with val of 1
 
-# gain of built
-
+# gain of built -- will be boolean image
 gain_of_built <- built_bool_85 != built_bool_71
 plot(gain_of_built)
 
+# Q1 Analysis:
+#######################################################
 
+# TOC Curve for Q1: Use TOC to describe the relationship between Gain of 
+# Built between 71-85 and distance from Built at 71
 my_TOC1 <- TOC(index = distance_71, 
                boolean = gain_of_built,
                mask = not_built_bool_71,
                NAval = 0,
                progress = T)
 plot(my_TOC1)
+
+# Q2 Data Prep:
+######################################################
+
+# reclassification order is important for this package
+reclassed_1971_matrix <- rbind(c(1L, 0L),
+                               c(2L, 1L),
+                               c(3L, 2L),
+                               c(4L, 3L))
+reclassed_1971 <- reclassify(lc_1971, rcl = reclassed_1971_matrix) #new index image
+
+# Q2 Analysis:
+#####################################################
+
+# Use the TOC to describe the relationship between Gain of Built
+# during 71-85 and land cover in 71
+
+my_TOC2 <- TOC(index = reclassed_1971, 
+               boolean = gain_of_built,
+               mask = not_built_bool_71,
+               NAval = 0,
+               progress = T)
+
+plot(my_TOC2)
+
+
+
+
 
 
